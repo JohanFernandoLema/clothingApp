8b1a812326114bb6dc05585a3bf3eeb734ab411c
JohanFernandoLema <johan.13fernando@gmail.com>
2023-05-27 17:09:50 -0400

    outlet component added to the App component in order to nest our pages within it

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,39 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom'
 import Product from './pages/Product/Product'
 import Products from './pages/Products/Products'
 import Home from './pages/Home/Home'
+import Navbar from './components/Navbar/Navbar'
+import Footer from './components/Footer/Footer'
+
+const Layout = () => {
+  return (
+    <div>
+      <Navbar />
+      {/* Outlet is nesting all my pages within itself */}
+      <Outlet />
+      <Footer />
+    </div>
+  )
+}
 function App() {
   const router = createBrowserRouter([
     {
       path: '/',
-      element: <Home />,
-    },
-    {
-      path: '/products/:id',
-      element: <Products />,
-    },
-    {
-      path: '/product/:id',
-      element: <Product />,
+      element: <Layout />,
+      children: [
+        {
+          path: '/',
+          element: <Home />,
+        },
+        {
+          path: '/products/:id',
+          element: <Products />,
+        },
+        {
+          path: '/product/:id',
+          element: <Product />,
+        },
+      ],
     },
   ])
   return (
